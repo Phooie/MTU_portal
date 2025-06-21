77e69bcde7c429dc45c5f2b42d8491c2e77c24eb
Phooie <140931826+Phooie@users.noreply.github.com>
2025-06-21 23:02:11 +0630

    Update settings.py

--- djangodb/settings.py
+++ djangodb/settings.py
@@ -36,7 +36,11 @@ SECRET_KEY = "django-insecure-*6ev4++3t1by%%(syh*dd3qoa(nnyaybz9c4t6lsx1gr!irdj^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'your-app-name.up.railway.app',
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
